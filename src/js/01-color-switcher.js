a806b502426adbc85fe17aa5d15184d85fc8ff08
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-11-09 17:16:44 +0200

    9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
-
+const btnStart = document.querySelector('button[data-start]'),
+  btnStop = document.querySelector('button[data-stop]');
+let r = 0;
+function changeColor() {
+  document.body.style.backgroundColor = `#${Math.floor(
+    16777215 * Math.random()
+  ).toString(16)}`;
+}
+btnStart.addEventListener('click', function () {
+  (r = setInterval(changeColor, 1000)),
+    btnStart.setAttribute('disabled', 'true'),
+    btnStop.removeAttribute('disabled', 'true');
+}),
+  btnStop.addEventListener('click', function () {
+    clearInterval(r),
+      btnStop.setAttribute('disabled', 'true'),
+      btnStart.removeAttribute('disabled', 'true');
+  });
