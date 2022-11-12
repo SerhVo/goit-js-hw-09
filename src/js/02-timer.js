3196166fcd41f061d01452b64dca8beac9d5f30d
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-11-12 19:00:31 +0200

    9.2 fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -41,7 +41,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
 
     if (selectedDates[0] < new Date()) {
       refs.start.disabled = true;
